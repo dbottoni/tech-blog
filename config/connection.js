6be0f4d36f833ce23ef2fc3eec7c0a4a8aa69bdb
Dana Bottoni <danabottoni@gmail.com>
2021-08-17 16:41:02 -0400

    more file structuring

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('tech_news_blog', 'username', 'password', {
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
